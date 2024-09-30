8dbc1e82cb69d1fe281e5421448833990432575b
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-30 02:58:30 +0200

    [FEAT] created PageController for the ' home' view. Created the 'app' view.

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="d-flex justify-content-end">
+                        additional menu
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Comics">
+                        </div>
+
+                        <div class="header_navbar">
+                            <ul>
+                                <li>
+                                    <a href="{{ route('homepage') }}">
+                                        HOME
+                                    </a>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
