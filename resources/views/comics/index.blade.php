bf1d921fe890b6d553c2926009612eeced33992d
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-30 14:25:12 +0200

    [FEAT] created show CRUD for comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,20 +9,22 @@
                 @foreach ($comics as $comic)
                     <div class="col-2">
                         <div class="pb-4">
+                            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">
 
-                            <div class="comics_img">
-                                <img class="img-fluid" src="https://picsum.photos/200" alt="">
-                            </div>
+                                <div class="comics_img">
+                                    <img class="img-fluid" src="https://picsum.photos/200" alt="">
+                                </div>
 
-                            <div class="comics_title">
-                                <h6>
-                                    {{ strtoupper($comic->title) }}
-                                    {{ strtoupper($comic->description) }}
-                                    {{ strtoupper($comic->price) }}
-                                    {{ strtoupper($comic->sale_date) }}
-                                    {{ strtoupper($comic->type) }}
-                                </h6>
-                            </div>
+                                <div class="comics_title">
+                                    <h6>
+                                        {{ strtoupper($comic->title) }}
+                                        {{ strtoupper($comic->description) }}
+                                        {{ strtoupper($comic->price) }}
+                                        {{ strtoupper($comic->sale_date) }}
+                                        {{ strtoupper($comic->type) }}
+                                    </h6>
+                                </div>
+                            </a>
 
                         </div>
                     </div>
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="py-5">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+
+                    <img class="img-fluid" src="https://picsum.photos/900" alt="">
+
+                </div>
+                <div class="col-6">
+
+                    <h1>{{ strtoupper($comic['title']) }}</h1>
+                    <p>{{ strtoupper($comic['description']) }}</p>
+                    <p>{{ strtoupper($comic['sale_date']) }}</p>
+                    <p>{{ strtoupper($comic['type']) }}</p>
+                    <p>{{ strtoupper($comic['price']) }}</p>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+@endsection
